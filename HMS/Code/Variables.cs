9a6eedbda13a180918dbe135e2f4674a19bde88b
Saman <samaneghbali1@gmail.com>
2022-10-14 11:57:38 -0400

    Add project files.

---
+++ HMS/Code/Variables.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HMS.Code
+{
+    public class Variables
+    {
+        public const string HotelFullName = "Marina Bay International Resort";
+        public const string HotelShortName = "Marina Bay";
+        public const string HotelNameAbbreviate = "MBIS";
+    }
+}
